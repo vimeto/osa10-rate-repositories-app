8b5db728fdb139c77d73ffed3e2fadb4dbce301f
Vilhelm Toivonen <vilhelm.toivonen@gmail.com>
2022-01-09 18:11:31 +0200

    Bug fixes

--- src/hooks/useRepositories.js
+++ src/hooks/useRepositories.js
@@ -1,10 +1,8 @@
 import { useState, useEffect } from 'react';
 import { useQuery } from '@apollo/client';
 import { GET_REPOSITORIES } from '../components/graphql/queries';
-import { useDebounce } from 'use-debounce';
 
 const useRepositories = (sort_values, searchKeyword) => {
-  // const [value] = useDebounce(searchKeyword, 500);
   const value = searchKeyword;
   const [orderBy, orderDirection] = sort_values ? sort_values.split(" ") : [undefined, undefined];
   const [repositories, setRepositories] = useState();
